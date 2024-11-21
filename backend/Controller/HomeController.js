3665a29aabde153f2023970e1ee604201f65b169
VyVictory <vyvictory2003@gmail.com>
2024-11-21 11:30:16 +0700

    env port database , ghi chu readme

--- backend/Controller/HomeController.js
+++ backend/Controller/HomeController.js
@@ -1,6 +1,7 @@
 const getHomePage = (req, res) => {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     return res.render("main",
 =======
     return res.render("index",
@@ -8,10 +9,13 @@ const getHomePage = (req, res) => {
 =======
     return res.render("home",
 >>>>>>> parent of f7b9ad7 (thêm file main , sửa lại render home .)
+=======
+    return res.render("main",
+>>>>>>> parent of 5d97e1b (chú í cập nhật file .env trong readme)
         {
             title: 'Home website',
             page: 'home',
         }
     )
 }
-export default getHomePage
+export default getHomePage;
