0d503fcc6c363071155968f76bc9c3482a19c0ee
VyVictory <vyvictory2003@gmail.com>
2024-11-21 11:33:52 +0700

    fix file homecontdroller

--- backend/Controller/HomeController.js
+++ backend/Controller/HomeController.js
@@ -1,17 +1,5 @@
 const getHomePage = (req, res) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     return res.render("main",
-=======
-    return res.render("index",
->>>>>>> parent of c4d97ad (chú í cập nhật .env , trong file readm)
-=======
-    return res.render("home",
->>>>>>> parent of f7b9ad7 (thêm file main , sửa lại render home .)
-=======
-    return res.render("main",
->>>>>>> parent of 5d97e1b (chú í cập nhật file .env trong readme)
         {
             title: 'Home website',
             page: 'home',
